1895b8855ab68c2eb0042bd4983adc59b656d32d
hhmaster <hamstermar@gmail.com>
2016-08-04 23:42:56 +0100

    partma index

---
+++ partma.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<script type="text/javascript" charset="utf-8" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="bt/js/bootstrap.min.js"></script>
+<link type="text/css" href="bt/css/bootstrap.min.css" rel="stylesheet"/>
+<link type="text/css" href="css/partma.css" rel="stylesheet" />
+<title>partma</title>
+<style type="text/css"> 
+</style>
+</head>
+<body>	
+	<div class="page-header clearfix bg-primary" style="margin:0;" >
+			<button style="font-weight:bold;font-size:18px;float:left;font-familty:ubuntu;color:white;"class="btn btn-lg btn-link">Partma.ma 
+		<!--small dir="rtl" lang="ar" class="droid-arabic-kufi">أحسن سمسار كاين فسوق</small --></button>
+		
+			<button	class="btn btn-lg btn-link" style="float:right;color:white"><span class="glyphicon glyphicon-menu-hamburger"></span></button>
+			<button	class="btn btn-lg btn-link" style="float:right;color:white"><span class="glyphicon glyphicon-user"></span></button>
+		
+	</div>	
+	<center>
+	<div style="margin-top:20%;" class="center-block">	
+	<button id="btnrent" class="droid-arabic-kufi btn btn-default" style="padding:16px;">باغي نكري</button>
+	<button id="btnchri" class="droid-arabic-kufi btn btn-default" style="padding:16px;">باغي نشري</button>
+	<br><button id='hvh' class="droid-arabic-kufi btn btn-link" style="padding:16px;">عندي دار لكرا/للبيع</button>
+	<br><button id='lgn' dir="rtl" lang="ar" class="droid-arabic-kufi btn btn-info btn-md " style="padding:16px;"><span class="glyphicon glyphicon-user"></span> نحل كونط جديد</button>
+	</div>
+	</center>
+</body>
+</html>
